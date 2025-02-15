c4d8f051193a0230c960a107ae3e60a09f6d2761
nishiuriraku <nishiuriraku@gmail.com>
2025-02-15 17:45:22 +0900

    add share link

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,17 @@
     <v-main>
       <Main />
     </v-main>
+    <v-footer class="bg-grey-lighten-2">
+      <v-btn
+        class="mx-2"
+        color="black"
+        rounded="xl"
+        variant="text"
+        href="https://x.com/intent/post?text=%E4%BB%8A%E3%83%8F%E3%83%A0%E3%83%81%E3%81%AE%E3%83%9C%E3%82%A4%E3%82%B9%E3%82%92%E8%81%B4%E3%81%84%E3%81%A6%E3%81%84%E3%81%BE%E3%81%99%EF%BC%81+%23%E3%83%8F%E3%83%A0%E3%83%81%E3%83%BC%E3%82%BA+%23NowPlaying+%0D%0Ahttps%3A%2F%2Fhamuchi-voice.netlify.app"
+        target="_blank"
+      >
+        Xにシェアする
+      </v-btn>
+    </v-footer>
   </v-app>
 </template>
