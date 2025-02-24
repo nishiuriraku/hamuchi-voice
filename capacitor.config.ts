6215a28bd104e7ba89ad079b16dd28be59577fc7
nishiuriraku <nishiuriraku@gmail.com>
2025-02-24 22:09:49 +0900

    add capacitor build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.hamuchi.app',
   appName: 'ハムチサンプラー',
-  webDir: 'dist'
+  webDir: 'dist',
 };
 
 export default config;
